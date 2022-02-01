1b0a0951ed0d9aa8a40ac23dd60536af69186727
ahuja <57448285+khushi-ahuja@users.noreply.github.com>
2022-02-01 17:56:41 +0530

    #450

---
+++ #450. Delete Node in a BST.java
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/delete-node-in-a-bst/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     
+    private int min(TreeNode root)
+    {
+        if(root.left == null)
+            return root.val;
+        return min(root.left);
+    }
+    public TreeNode deleteNode(TreeNode root, int val) {
+        if( root == null )
+            return null;
+        if(val < root.val)
+            root.left = deleteNode(root.left, val);
+        else if( val > root.val)
+            root.right = deleteNode(root.right, val);
+        else
+        {
+            if( root.left == null && root.right == null )
+                return null;
+            else if( root.right == null)
+                return root.left;
+            else if( root.left == null)
+                return root.right;
+            else
+            {
+                int inorderSucc = min(root.right);
+                root.val = inorderSucc;
+                root.right = deleteNode(root.right, inorderSucc);
+            }
+        }
+        return root;
+    }
+   
+}
